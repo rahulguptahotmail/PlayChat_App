62024a90c260f6a6b097e758e960870b9004f88e
Rahul Gupta <154514374+rahulguptahotmail@users.noreply.github.com>
2025-02-13 09:52:52 +0530

    Update Home.js

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import HomeVideo from "../utils/homeUtils/HomeVideo";
 
 const Home = () => {
   const [items, setItems] = useState([]);
+  const loaderRef = useRef(null);
 
   useEffect(() => {
     const fetchVideos = async () => {
       const res = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL}api/v1/video/videos`
       );
+      loaderRef.current.classList.add("d-none");
       setItems(res.data.videos);
     };
 
@@ -21,8 +23,9 @@ const Home = () => {
       id="homeVideos"
       className="d-flex flex-wrap ms-md-4 ps-md-5 pt-2 text-center border"
     >
-      <div id="loader" className=" w-100 d-none">
+      <div id="loader" className=" w-100 fw-bold p-3" ref={loaderRef}>
         <div className="loader m-auto"></div>
+        Loading...
       </div>
       {items.map((item, idx) => (
         <HomeVideo index={idx} video={item} />
