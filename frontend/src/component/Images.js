5ed03ad350927fa270f77dc61f7e40caf582f448
Rahul Gupta <154514374+rahulguptahotmail@users.noreply.github.com>
2025-02-13 10:01:34 +0530

    Update Images.js

--- frontend/src/component/Images.js
+++ frontend/src/component/Images.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import CommentBox from "../utils/universalUtils/CommentBox";
 import axios from "axios";
 import { Link } from "react-router-dom";
